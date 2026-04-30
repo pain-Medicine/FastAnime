b63b89008dd19894e1fa1d259f0eddbc1e475015
Dave Benton <dbenton@itcurves.net>
2026-04-30 11:01:54 -0400

    Remove temporary referer fix utility scripts

--- fix_referers.py
+++
@@ -1,8 +0,0 @@
-import os, glob
-for f in glob.glob('viu_media/libs/provider/anime/allanime/extractors/*.py'):
-    content = open(f).read()
-    if 'headers={"Referer":' in content:
-        content = content.replace('https://{API_BASE_URL}/', '{API_GRAPHQL_REFERER}')
-        content = content.replace('API_BASE_URL', 'API_BASE_URL, API_GRAPHQL_REFERER')
-        open(f, 'w').write(content)
-        print("Fixed", f)
